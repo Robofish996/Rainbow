6dd3eee4e600d60dccb87e4999c183107f3f835a
Robofish996 <114180692+Robofish996@users.noreply.github.com>
2023-02-13 20:17:46 +0000

    Completed Interactive window(basic)

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function myRainbow() {
+    let colorOne = document.getElementById("red");
+    colorOne.style.backgroundColor = "red";
+    let colorTwo = document.getElementById("orange");
+    colorTwo.style.backgroundColor = "orange";
+    let colorThree = document.getElementById("yellow");
+    colorThree.style.backgroundColor = "yellow";
+    let colorFour = document.getElementById("pink");
+    colorFour.style.backgroundColor = "pink";
+    let colorFive = document.getElementById("green");
+    colorFive.style.backgroundColor = "green";
+    let colorSix = document.getElementById("blue");
+    colorSix.style.backgroundColor = "blue";
+    let colorSeven = document.getElementById("purple");
+    colorSeven.style.backgroundColor = "purple";
+  }
